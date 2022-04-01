4af67389c835edaba4e9fcc5b0dbf64dd9123af0
gmeplace <102834615+gmeplace@users.noreply.github.com>
2022-04-01 20:11:03 -0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/5nx8jy1.png";
+            i.src = "https://github.com/gmeplace/thing/raw/main/template.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
