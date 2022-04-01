d067117ab5613307ac792cd2954d98c032b70c8d
gmeplace <102834615+gmeplace@users.noreply.github.com>
2022-04-01 19:10:41 -0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         gme Logo template
+// @name         SuperStonk Logo template
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the canvas!
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
+            i.src = "https://i.imgur.com/AM2xcuI.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
